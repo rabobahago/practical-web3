aebee5e75632eae938b0bac96605e32efc44e26d
rabobahago <rabobahago@gmail.com>
2023-09-04 12:00:38 +0100

    transferable wei solidity code added

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -5,7 +5,17 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Counter {
+    address public addressOne;
+    address public addressTwo;
     int public counter;
+
+    address public owner;
+    address public charity;
+
+    constructor(address _charity) {
+        owner = msg.sender;
+        charity = _charity;
+    }
 
     function get() public view returns (int) {
         return counter;
@@ -21,5 +31,23 @@ contract Counter {
 
     function zero() public {
         counter = 0;
+    }
+
+    receive() external payable {
+        uint totalValue = msg.value;
+        (bool successOne, ) = addressOne.call{value: totalValue / 2}("");
+        require(successOne);
+        (bool successTwo, ) = addressTwo.call{value: totalValue / 2}("");
+        require(successTwo);
+    }
+
+    function donate() external payable {
+        (bool success, ) = charity.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function tip() external payable {
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success);
     }
 }
