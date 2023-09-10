3b68847b5470c4d78fc30fe21dc040c7357f7e9f
rabobahago <rabobahago@gmail.com>
2023-09-10 12:45:57 +0100

    conditions for deploying multisig contract added

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -6,6 +6,9 @@ contract MultiSig {
     uint public required;
 
     constructor(address[] memory _owners, uint _required) {
+        require(_owners.length > 0, "Required  more than zero address");
+        require(_required > 0);
+        require(_required <= _owners.length);
         owners = _owners;
         required = _required;
     }
