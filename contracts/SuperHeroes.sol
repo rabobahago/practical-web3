921030fe62316230056d0fa28f91e0775c005cc0
rabobahago <rabobahago@gmail.com>
2023-09-10 23:13:22 +0100

    attack for both Mage and Warrior implemented

--- contracts/SuperHeroes.sol
+++ contracts/SuperHeroes.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Hero {
+interface Enemy {
+    function takeAttack(Hero.AttackTypes attackType) external;
+}
+
+abstract contract Hero {
     uint public health = 100;
 
     constructor(uint _health) {
@@ -11,8 +15,25 @@ contract Hero {
     function takeDamage(uint damage) external {
         health -= damage;
     }
+
+    enum AttackTypes {
+        Brawl,
+        Spell
+    }
+
+    function attack(address enemy) public virtual;
 }
 
-contract Mage is Hero(100) {}
+contract Mage is Hero(100) {
+    function attack(address _add) public override {
+        Enemy enemy = Enemy(_add);
+        enemy.takeAttack(Hero.AttackTypes.Spell);
+    }
+}
 
-contract Warrior is Hero(50) {}
+contract Warrior is Hero(50) {
+    function attack(address _add) public override {
+        Enemy enemy = Enemy(_add);
+        enemy.takeAttack(Hero.AttackTypes.Brawl);
+    }
+}
