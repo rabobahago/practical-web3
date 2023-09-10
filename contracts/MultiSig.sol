9996c3ae1dab02a9f249bb06fc22516e085c5559
rabobahago <rabobahago@gmail.com>
2023-09-10 14:28:29 +0100

    add transaction function added

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.9;
 contract MultiSig {
     address[] public owners;
     uint public required;
+    uint public transactionCount;
+
+    struct Transaction {
+        address destinations;
+        uint value;
+        bool executed;
+    }
+    mapping(uint256 => Transaction) public transactions;
 
     constructor(address[] memory _owners, uint _required) {
         require(_owners.length > 0, "Required  more than zero address");
@@ -11,5 +19,15 @@ contract MultiSig {
         require(_required <= _owners.length);
         owners = _owners;
         required = _required;
+    }
+
+    function addTransaction(
+        address _destination,
+        uint _value
+    ) public returns (uint) {
+        uint transactionId = transactionCount;
+        transactions[transactionId] = Transaction(_destination, _value, false);
+        transactionCount += 1;
+        return transactionId;
     }
 }
