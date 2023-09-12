255ffb648a97578dd2533a8bc15c9de9ccf7c07f
rabobahago <rabobahago@gmail.com>
2023-09-12 21:17:52 +0100

    balanceOf function added

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.9;
 
 contract Token {
-
     string public name = "Falafel";
-	string public symbol = "FLF";
-	uint8 public decimals = 18;
+    string public symbol = "FLF";
+    uint8 public decimals = 18;
     uint public totalSupply;
+
+    mapping(address => uint) public balance;
+
+    function balanceOf(address _add) public returns (uint) {
+        return balance[_add];
+    }
 }
