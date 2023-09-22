b07e703a82fdd071718a38cfebba5ad5ea7bc444
rabobahago <rabobahago@gmail.com>
2023-09-22 23:22:54 +0100

    zombie factory contract added

---
+++ contracts/zombies/ZombieFactory.sol
@@ -0,0 +1,50 @@
+/**By value, which means that the Solidity compiler creates a new copy of the parameter's value and passes it to your function. This allows your function to modify the value without worrying that the value of the initial parameter gets changed.
+ /*
+
+ /**
+  @ By reference, which means that your function is called with a... reference to the original variable. Thus, if your function changes the value of the variable it receives, the value of the original variable gets changed.
+  */
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+    event NewZombie(uint zombieId, string name, uint dna);
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        /**By value, which means that the Solidity compiler creates a new copy of the parameter's value and passes it to your function. This allows your function to modify the value without worrying that the value of the initial parameter gets changed. This achieved with word like memory, calldata
+       /*
+
+       /**
+        @ By reference, which means that your function is called with a... reference to the original variable. Thus, if your function changes the value of the variable it receives, the value of the original variable gets changed.
+        */
+        uint id = zombies.push(Zombie(_name, _dna));
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(
+        string memory _str
+    ) private view returns (uint) {
+        // start here
+    }
+
+    function _generateRandomDna(
+        string memory _str
+    ) private view returns (uint) {
+        // start here
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
