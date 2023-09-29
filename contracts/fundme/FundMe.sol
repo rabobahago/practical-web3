fa5cc697ddcb2d6c06dcf3c603aba7c67a147ad4
rabobahago <rabobahago@gmail.com>
2023-09-29 08:26:08 +0100

    FundMe contract completed

--- contracts/fundme/FundMe.sol
+++ contracts/fundme/FundMe.sol
@@ -3,15 +3,25 @@ pragma solidity ^0.8.8;
 import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
     uint minimumUsd = 50 * 1e18;
     address[] public funders;
     mapping(address => uint) public addressToAmountFunded;
+    address owner;
 
     /**
      * Network: Sepolia
      * Data Feed: ETH/USD
      * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
      */
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOnwer() {
+        require(msg.sender == owner, "Sender is not owner");
+        _;
+    }
 
     function fund() public payable {
         require(
@@ -19,8 +29,23 @@ contract FundMe {
             "Didn't send enough!"
         );
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw(){}
+    function withdraw() public payable onlyOnwer {
+        for (
+            uint funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool callSuccessful, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccessful, "Call Failed");
+    }
 }
