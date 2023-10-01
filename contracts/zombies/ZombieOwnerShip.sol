72c53407905172d8f97eefd61d233d205e3cc348
rabobahago <rabobahago@gmail.com>
2023-10-01 18:19:14 +0100

    ERC721 contract implemented

--- contracts/zombies/ZombieOwnerShip.sol
+++ contracts/zombies/ZombieOwnerShip.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
+import "./ERC721.sol";
 
 // Start here
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
 }
