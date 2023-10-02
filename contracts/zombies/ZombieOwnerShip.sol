57a0bd7c6c44b830e74b13d6b7b8b4afcbd68167
rabobahago <rabobahago@gmail.com>
2023-10-02 23:29:36 +0100

    ownerOf and balanceOf ERC721 token implemented

--- contracts/zombies/ZombieOwnerShip.sol
+++ contracts/zombies/ZombieOwnerShip.sol
@@ -4,7 +4,22 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombieattack.sol";
 import "./ERC721.sol";
 
-// Start here
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    function balanceOf(address _owner) external view returns (uint256) {
+        // 1. Return the number of zombies `_owner` has here
+        return ownerZombieCount[_owner];
+    }
 
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        // 2. Return the owner of `_tokenId` here
+        return zombieToOwner[_tokenId];
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {}
+
+    function approve(address _approved, uint256 _tokenId) external payable {}
 }
